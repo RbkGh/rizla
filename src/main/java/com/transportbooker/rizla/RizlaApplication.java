e6a37ec40ccf61e2279829bf2cd0321695ef9b5f
Ace Rocks <PeswaAgent@Ace.local>
2024-08-25 08:40:58 +0000

    first commit

---
+++ src/main/java/com/transportbooker/rizla/RizlaApplication.java
@@ -0,0 +1,13 @@
+package com.transportbooker.rizla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RizlaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RizlaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/transportbooker/rizla/RizlaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transportbooker.rizla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RizlaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
